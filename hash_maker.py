3baf6760bcf2aa3d94f35ac2e08f4d9a88dfa21b
yousif nema <141075213+yousifnema@users.noreply.github.com>
2024-02-09 15:27:05 +0300

    Update hash_maker.py

--- hash_maker.py
+++ hash_maker.py
@@ -9,7 +9,7 @@ def search():
         for name in dirs:
             file_paths.append(os.path.join(root, name))
 
-    with open(r"hash.txt", "w") as text_file:
+    with open(r"hash.txt", "a") as text_file:
         for path in file_paths:
             try:
                 text_file.write(path + '\n')
